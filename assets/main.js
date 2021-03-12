87c918219c37cf161fa7915d773edd5d8f8c04bb
Adesuwa-o <adesuwa.osewa@gmail.com>
2021-03-12 23:31:22 +0000

    djjradio
    Added Dj and radio section to website.

--- assets/main.js
+++ assets/main.js
@@ -67,6 +67,12 @@ sr.reveal('.about_profession' , {delay: 400})
 sr.reveal('.about_text' , {delay: 500})
 sr.reveal('.about_social-icon' , {delay: 600, interval:200})
 
+/* scroll dj + radio*/
+sr.reveal('.djradio_img' , {delay: 500})
+sr.reveal('.djradio_subtitle' , {delay: 300})
+sr.reveal('.djradio_text' , {delay: 500})
+sr.reveal('.play' , {delay: 200}) 
+
 /* scroll skills*/
 
 sr.reveal('.skills_subtitle' , {})
